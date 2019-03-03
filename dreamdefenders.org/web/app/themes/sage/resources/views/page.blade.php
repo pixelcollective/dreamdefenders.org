469da6e2e04cb55993f7d57757560f966d70493b
Kelly Mears <developers@tinypixel.io>
2019-03-02 23:10:16 -0800

    ready to go

---
+++ dreamdefenders.org/web/app/themes/sage/resources/views/page.blade.php
@@ -0,0 +1,44 @@
+@extends('layouts.app')
+
+@section('content')
+  @posts
+
+    {{-- Builder Layout --}}
+    @if(get_field('components'))
+      @layouts('components')
+
+        @layout('header')
+          @include('components.hero')
+        @endlayout
+
+        @layout('content')
+        <div class="page bg-white">
+          @include('partials.content-page')
+        </div>
+        @endlayout
+
+        @layout('instagram')
+          @shortcode('[instagram-feed]')
+        @endlayout
+
+        @layout('form')
+          @include('components.form')
+        @endlayout
+
+        @layout('cta-email')
+          @include('partials.cta-email')
+        @endlayout
+
+      @endlayouts
+
+    {{-- Fallback Layout --}}
+    @else
+      @include('components.hero-default')
+      <div class="page bg-white">
+        @include('partials.content-page')
+      </div>
+      @shortcode('[instagram-feed]')
+    @endif
+
+  @endposts
+@endsection
