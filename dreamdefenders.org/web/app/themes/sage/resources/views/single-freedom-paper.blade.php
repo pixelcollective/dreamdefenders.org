153fb8b979e524fade4bd62a575c81a55194cb33
Kelly Mears <developers@tinypixel.io>
2019-03-03 22:00:43 -0800

    👍

--- dreamdefenders.org/web/app/themes/sage/resources/views/single-freedom-paper.blade.php
+++ dreamdefenders.org/web/app/themes/sage/resources/views/single-freedom-paper.blade.php
@@ -7,7 +7,7 @@
 @section('content')
   @posts
     <main>
-      <div class="page bg-white">
+      <div class="page bg-black">
         @include('components.post_nav')
         <section class="freedom-paper-single-view">
           <img src="@field('freedompaper__download', 'url')" style="width:100vw;" />
