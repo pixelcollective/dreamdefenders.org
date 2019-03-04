d598a2de80b4c88c54bd43aab0ce9e326dd0a9cb
Kelly Mears <developers@tinypixel.io>
2019-03-03 21:34:22 -0800

    freedom page

--- dreamdefenders.org/web/app/themes/sage/resources/views/single-freedom-paper.blade.php
+++ dreamdefenders.org/web/app/themes/sage/resources/views/single-freedom-paper.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('header')
-<div class="bg-black">
+<div class="bg-black" style="display: block;">
   @include('partials.header')
 </div>
 @endsection
@@ -11,10 +11,29 @@
   @posts
     <main>
       <div class="page bg-white">
+        <div class="fixed post_nav">
+          <div class="prev_post">
+            @php $prev = get_previous_post() @endphp
+            @if(is_a( $prev , 'WP_Post' ))
+              <a href="{!! get_permalink( $prev->ID ); !!}">
+                {!! get_the_title( $prev->ID ); !!}
+              </a>
+            @endif
+          </div>
+          <div class="next_post">
+              @php $next = get_next_post() @endphp
+              @if(is_a( $next , 'WP_Post' ))
+                <a href="{!! get_permalink( $next->ID ); !!}">
+                  {!! get_the_title( $next->ID ); !!}
+                </a>
+              @endif
+          </div>
+        </div>
         <section>
           <img src="@field('freedompaper__image', 'url')" style="width:100vw;" />
         </section>
       </div>
     </main>
   @endposts
+
 @endsection
