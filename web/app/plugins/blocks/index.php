5d99151fa63631eb51817aa3b557d346e2b4875c
Kelly Mears <developers@tinypixel.dev>
2020-01-12 23:18:41 -0800

    :nail_care: modify plugin manifest

--- web/app/plugins/blocks/index.php
+++ web/app/plugins/blocks/index.php
@@ -10,6 +10,7 @@ namespace TinyPixel\Blocks;
 require __DIR__ . '/vendor/autoload.php';
 
 $tinyblocks = \TinyBlocks\App::getInstance(__DIR__ . '/config');
-$tinyblocks->initialize();
 
-$tinyblocks->addBlock(\TinyPixel\Blocks\TwoColumn::class);
+$tinyblocks->addBlock(
+  \TinyPixel\Blocks\TwoColumn::class
+);
