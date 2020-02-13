1a6565c0730d1b059b5715dcf5020cf9ebafb2d0
Kelly Mears <developers@tinypixel.dev>
2020-02-12 23:45:06 -0800

    update single post template

--- web/app/themes/sage/app/setup.php
+++ web/app/themes/sage/app/setup.php
@@ -186,30 +186,6 @@ add_action('after_setup_theme', function () {
 }, 20);
 
 /**
- * Register the theme sidebars.
- *
- * @return void
- */
-add_action('widgets_init', function () {
-    $config = [
-        'before_widget' => '<section class="widget %1$s %2$s">',
-        'after_widget' => '</section>',
-        'before_title' => '<h3>',
-        'after_title' => '</h3>'
-    ];
-
-    register_sidebar([
-        'name' => __('Primary', 'sage'),
-        'id' => 'sidebar-primary'
-    ] + $config);
-
-    register_sidebar([
-        'name' => __('Footer', 'sage'),
-        'id' => 'sidebar-footer'
-    ] + $config);
-});
-
-/**
  * Standard Post Posttype
  */
 add_action('init', function () {
