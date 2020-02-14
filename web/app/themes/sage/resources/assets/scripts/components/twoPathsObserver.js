17fd27b55174a5292312a719ccefcea74cc1a87d
Kelly Mears <developers@tinypixel.dev>
2020-02-14 06:25:14 -0800

    homepage anime

--- web/app/themes/sage/resources/assets/scripts/components/twoPathsObserver.js
+++ web/app/themes/sage/resources/assets/scripts/components/twoPathsObserver.js
@@ -19,18 +19,123 @@ export default () => {
     return ! target.classList.contains(`is-animating`)
   }
 
-  const animate = (targets, width) => {
+  const animate = (targets, params) => {
     targets.classList.add(`is-animating`)
-
-    anime({ targets, width, ...animeOptions })
+    anime({ targets, ...animeOptions, ...params })
   }
 
   const observer = new IntersectionObserver(els => {
+    const cross = document.querySelector(`.exOut`)
+    const badThings = document.querySelector(`.two-paths-left`)
+    const badText = document.querySelector(`.two-paths-left h2`)
+    const dividerOr = document.querySelector(`.divider-or`)
+    const riseBg = document.querySelector(`.two-paths-right .we-rise-bg-container`)
+    const riseImage = document.querySelector(`.two-paths-right .we-rise-bg-container .we-rise-bg`)
+    const riseText = document.querySelector(`.two-paths-right h2`)
+
+    const minHeight = badThings && badThings.clientHeight;
+
     els.forEach(({ target, intersectionRatio }) => {
-      intersectionRatio > 0 && animate(target, `100%`)
+      intersectionRatio > 0 && (() => {
+        cross && animate(cross, {
+          width: `100%`,
+        })
+
+        badThings && animate(badThings, {
+          width: `0em`,
+          opacity: 0,
+          height: 0,
+        })
+
+        badText && animate(badText, {
+          opacity: 0,
+          duration: 1000,
+        })
+
+        dividerOr && animate(dividerOr, {
+          opacity: 0,
+          height: 0,
+          rotateX: `280deg`,
+          rotateZ: `180deg`,
+          duration: 300,
+        })
+
+        target && (() => {
+          animate(target, {
+            width: `100vw`,
+            height: minHeight,
+          })
+        })()
+
+        /**
+         * Rise background
+         */
+        riseBg && (() => {
+          animate(riseBg, {
+            opacity: 1,
+            duration: 1000,
+          })
+        })()
+
+        /**
+         * Rise background image
+         */
+        riseImage && (() => {
+          animate(riseImage, {
+            opacity: 1,
+            duration: 6000,
+            width: `100vw`,
+            height: minHeight,
+          })
+        })()
+
+        /**
+         * Rise text
+         */
+        riseText && (() => animate(riseText, {
+          fontSize: `4rem`,
+        }))()
+      })()
     })
   }, observerOptions)
 
-  const target = document.querySelector(`.two-paths-left`)
+  const target = document.querySelector(`.two-paths-right`)
+
   target && hasNotFired(target) && observer.observe(target)
+
+  const rise = document.querySelector(`.two-paths-right`)
+  const riseHighlight = document.querySelector(`.two-paths-right .rise-highlight`)
+  const riseText = document.querySelector(`.two-paths-right h2`)
+
+  rise.addEventListener(`mouseover`, () => {
+    anime({
+      targets: [riseText],
+      left: `1rem`,
+      easing: `easeInOutSine`,
+      duration: 800,
+    })
+
+    anime({
+      targets: [riseHighlight],
+      color: `rgba(253, 225, 53, 1)`,
+      duration: 800,
+      easing: `easeInOutSine`,
+    })
+  })
+
+  rise.addEventListener(`mouseout`, () => {
+    anime({
+      targets: [riseText],
+      left: `0rem`,
+      easing: `easeInOutSine`,
+      duration: 800,
+    })
+
+    anime({
+      targets: [riseHighlight],
+      color: `rgba(0, 0, 0, 1)`,
+      duration: 800,
+      easing: `easeInOutSine`,
+    })
+  })
 }
--- web/app/themes/sage/resources/views/partials/front/two-paths.blade.php
+++ web/app/themes/sage/resources/views/partials/front/two-paths.blade.php
@@ -1,30 +1,34 @@
-<div class="bg-black">
+<div class="hidden lg:block bg-black">
   <div class="container mx-auto">
-    <div class="flex flex-col justify-around lg:flex-row">
-      <div class="flex-1 hidden w-full overflow-hidden text-black lg:w-1/5">
-        @svg('waves-triple', 'fill-current max-w-4/5')
-      </div>
-
-      <div class="w-full lg:w-3/5">
-        <h2 class="text-4xl font-bold text-center text-white">
+    <div class="flex flex-col justify-around lg:flex-row relative z-20 bg-black">
+      <div class="w-3/5 mx-auto relative h-16">
+        <h2 class="text-4xl font-bold text-center mx-auto block w-3/5 text-white relative">
           There are two paths
+          <div class="exOut bg-white w-0 h-4 absolute" style="top: 40%;"></div>
         </h2>
-      </div>
-
-      <div class="flex-1 hidden overflow-hidden text-white lg:w-1/5">
-        @svg('waves-triple', 'fill-current max-w-4/5')
       </div>
     </div>
   </div>
 
-  <div class="relative h-64">
-    <div class="two-paths-left hidden md:inline-block h-64 w-1/2 overflow-hidden text-white bg-white">
-      {{-- foo --}}
+  <div class="flex flex-row overflow-hidden relative">
+    <div class="divider-or absolute inline-block bg-black text-white z-30 uppercase font-bold px-2 py-1" style="top: 40%; left: 48%;">
+      Or
     </div>
 
-    <div class="two-paths-right hidden md:inline-block h-64 w-auto max-w-1/2 overflow-hidden text-white bg-black">
-      {{-- baz --}}
+    <div class="two-paths-left hidden md:inline-block h-full w-auto w-1/2 max-w-1/2 text-white bg-black">
+      <h2 class="text-white font-display text-2xl uppercase p-16">
+        Rising gun violence, tuition, unemployment, the number of our people dead, deported or in jail, rent, stress, debt, overdoses, suicide, hate.
+      </h2>
+    </div>
+
+    <div class="two-paths-right hidden md:inline-block w-auto w-1/2 text-white bg-white overflow-hidden relative">
+      <div class="we-rise-bg-container absolute w-full left-0 right-0 bottom-0 top-0 bg-white">
+        <img src="@asset('images/we-rise-bg.jpg')" class="we-rise-bg relative object-cover absolute top-0" style="opacity: 0; width: 300vw;" />
+      </div>
+
+      <h2 class="text-black font-display text-2xl uppercase italic p-16 absolute top-0 left-0 right-0 bottom-0 z-10">
+        We <span class="rise-highlight">Rise.</span>
+      </h2>
     </div>
   </div>
 </div>
-
