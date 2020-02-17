2a67dd8541fc0192b29eb70118281e973f217fa1
Kelly Mears <developers@tinypixel.dev>
2020-02-17 01:11:17 -0800

    upgrades for freedom papers block

--- web/app/themes/sage/resources/views/partials/front/freedom-papers-teaser.blade.php
+++ web/app/themes/sage/resources/views/partials/front/freedom-papers-teaser.blade.php
@@ -1,4 +1,5 @@
-<div class="bg-gradient-b-white-yellow py-16">
+<div class="bg-gradient-b-white-yellow py-16 relative">
+  <img src="@asset('images/world-we-are-fighting-for.png')" class="absolute right-0 top-0 hidden lg:block lg:h-64" style="right: 2rem; top: 25%;" />
   <div class="container mx-auto px-4">
     <div class="flex flex-wrap flex-col md:flex-row overflow-hidden content-center">
       <div class="w-full md:w-1/2 overflow-hidden text-center md:text-left">
@@ -6,9 +7,12 @@
       </div>
 
       <div class="w-full md:w-1/2 overflow-hidden flex flex-col align-end px-2 text-center md:text-right align-middle self-center content-center">
-          <h2 class="text-xl inline w-auto">
-            <span class="border-b-2 border-blue border-blue">Freedom Papers</span>
-          </h2>
+        <h2 class="text-2xl inline w-auto relative">
+          @svg('arrow-right', 'inline h-12 w-12 relative top-0 left-0 right-0', ['width' => 32, 'height' => 32])
+          <span class="border-b-4 absolute bottom-0 border-blue h-4 w-full" style="bottom: 20%"></span>
+          <span>Freedom Papers</span>
+        </h2>
+
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
       </div>
     </div>
