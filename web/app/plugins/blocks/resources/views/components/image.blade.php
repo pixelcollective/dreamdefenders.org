0a0b9d2870e7f812ed8ec76729688d57b606821a
Kelly Mears <developers@tinypixel.dev>
2020-02-17 13:33:19 -0800

    conservative image

--- web/app/plugins/blocks/resources/views/components/image.blade.php
+++ web/app/plugins/blocks/resources/views/components/image.blade.php
@@ -1,18 +1,20 @@
-<div class="{!! isset($classes) ? $classes : 'w-full pb-6 pr-0 lg:pr-8 md:pb-0 md:max-w-4/5' !!}">
-  <picture>
-    <source
-      media="(min-width: {{ absint(wp_get_attachment_image_src($image, 'full')) }}px)"
-      srcset="{{ wp_get_attachment_image_srcset($image, 'full') }}"
-      sizes="{{ wp_get_attachment_image_sizes($image, 'full') }}" />
-    <img
-      srcset="{{ esc_attr(wp_get_attachment_image_srcset($image, 'full')) }}"
-      alt="{{ esc_attr(get_post_meta($image, '_wp_attachment_image_alt', true)) }}"
-      sizes="{{ esc_attr(wp_get_attachment_image_sizes($image, 'full')) }}" />
-  </picture>
-</div>
+@isset($image)
+  <div class="{!! isset($classes) ? $classes : 'w-full pb-6 pr-0 lg:pr-8 md:pb-0 md:max-w-4/5' !!}">
+    <picture>
+      <source
+        media="(min-width: {{ absint(wp_get_attachment_image_src($image)) }}px)"
+        srcset="{{ wp_get_attachment_image_srcset($image) }}"
+        sizes="{{ wp_get_attachment_image_sizes($image) }}" />
+      <img
+        srcset="{{ esc_attr(wp_get_attachment_image_srcset($image)) }}"
+        alt="{{ esc_attr(get_post_meta($image, '_wp_attachment_image_alt', true)) }}"
+        sizes="{{ esc_attr(wp_get_attachment_image_sizes($image)) }}" />
+    </picture>
+  </div>
+@endisset
 
 @isset($debug)
   @if($debug)
-    @dump($debug)
+    @dump($image)
   @endif
 @endisset
