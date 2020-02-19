f697b4b149337be80205a066f06b53e1ff8cd0d5
Kelly Mears <developers@tinypixel.dev>
2020-02-19 06:35:51 -0800

    add manifest.json

--- web/app/themes/sage/resources/views/partials/head.blade.php
+++ web/app/themes/sage/resources/views/partials/head.blade.php
@@ -1,6 +1,16 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <meta name="mobile-web-app-capable" content="yes">
+  <meta name="apple-mobile-web-app-capable" content="yes">
+  <meta name="application-name" content="DREAM">
+  <meta name="apple-mobile-web-app-title" content="DD">
+  <meta name="theme-color" content="#fde135">
+  <meta name="msapplication-navbutton-color" content="#fde135">
+  <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
+  <meta name="msapplication-starturl" content="{!! $app->site->url !!}">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link rel="manifest" href="manifest.json">
+
   @php(wp_head())
 </head>
