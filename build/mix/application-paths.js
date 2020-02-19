989f6a7a12655ea95b10927b41c0541e2e059c00
Kelly Mears <developers@tinypixel.dev>
2020-02-19 06:29:54 -0800

    fix cache busting

--- build/mix/application-paths.js
+++ build/mix/application-paths.js
@@ -1,20 +1,18 @@
-const theme     = file => `./web/app/themes/sage/${file}`,
-      blocks    = file => `./web/app/plugins/blocks/${file}`
+/** Helpers */
+const theme  = file => `./web/app/themes/sage/${file}`,
+      blocks = file => `./web/app/plugins/blocks/${file}`
 
+/** Export pathing util */
 module.exports = {
   gutenberg: {
     dist: file => `./web/app/plugins/gutenberg/build/${file}`,
   },
   theme: {
     src:    file => theme(`resources/assets/${file}`),
-    script: file => theme(`resources/assets/scripts/${file}`),
-    style:  file => theme(`resources/assets/styles/${file}`),
     dist:   file => theme(`dist/${file}`),
   },
   blocks: {
-    src:    file => blocks(`resources/${file}`),
-    script: file => blocks(`resources/assets/scripts/${file}`),
-    style:  file => blocks(`resources/assets/styles/${file}`),
+    src:    file => blocks(`resources/assets/${file}`),
     dist:   file => blocks(`dist/${file}`),
   },
   purgeWatch: [
--- build/mix/mix.js
+++ build/mix/mix.js
@@ -10,35 +10,33 @@ require('laravel-mix-tweemotional')
 require('laravel-mix-copy-watched')
 
 const devUrl = `http://dreamdefenders.vagrant`
-const { theme, blocks, gutenberg, purgeWatch } = require(`mix/application-paths`)
+const {
+  theme,
+  blocks,
+  gutenberg,
+  purgeWatch,
+} = require(`mix/application-paths`)
 
 module.exports = () => {
-  /**
-   * Tooling
-   */
-  mx.webpackConfig({
+  mx.setPublicPath(`./web/app/themes/sage/dist`)
+    .setResourceRoot(`./web/app`)
+    .browserSync(devUrl)
+    .webpackConfig({
       plugins: [
         new wpDeps({
           injectPolyfill: false,
         }),
       ],
-    }).babelConfig({
-      presets: ['@wordpress/babel-preset-default'],
-    }).options({
-      postCss: [tw],
-      processCssUrls: false,
-    }).tweemotional()
-    .setPublicPath(`./web/app/themes/sage/dist`)
-    .setResourceRoot(`./web/app`)
-    .browserSync(devUrl)
+    })
+    .babelConfig({ presets: ['@wordpress/babel-preset-default'] })
+    .options({ postCss: [tw], processCssUrls: false })
+    .tweemotional()
 
-  /**
-   * Styles
-   */
-  mx.sass(theme.style(`app.scss`), theme.dist(`styles`))
-    .sass(theme.style(`editor.scss`), theme.dist(`styles`))
-    .sass(blocks.style(`public.scss`), theme.dist(`styles`))
-    .sass(blocks.style(`editor.scss`), theme.dist(`styles/editor.css`))
+  /** Application styles */
+  mx.sass(theme.src(`styles/app.scss`), theme.dist(`styles`))
+    .sass(theme.src(`styles/editor.scss`), theme.dist(`styles`))
+    .sass(blocks.src(`styles/public.scss`), theme.dist(`styles`))
+    .sass(blocks.src(`styles/editor.scss`), theme.dist(`styles/editor.css`))
     .postCss(gutenberg.dist(`block-library/style.css`), theme.dist(`styles/gutenberg`))
     .purgeCss({
       enabled: true,
@@ -48,30 +46,25 @@ module.exports = () => {
       whitelistPatternsChildren: whitelist,
     });
 
-  /**
-   * Scripts
-   */
-  mx.js(theme.script(`app.js`), theme.dist(`scripts`))
+  /** Application scripts */
+  mx.js(theme.src(`scripts/app.js`), theme.dist(`scripts`))
+    .js(theme.src(`scripts/editor.js`), theme.dist(`scripts`))
+    .js(blocks.src(`scripts/blocks/Banner/block.js`), blocks.dist(`scripts/banner`))
+    .js(blocks.src(`scripts/blocks/Container/block.js`), blocks.dist(`scripts/container`))
+    .js(blocks.src(`scripts/blocks/FreedomPaper/block.js`), blocks.dist(`scripts/freedom-paper`))
+    .js(blocks.src(`scripts/blocks/HorizontalCard/block.js`), blocks.dist(`scripts/horizontal-card`))
+    .js(blocks.src(`scripts/blocks/TwoColumn/block.js`), blocks.dist(`scripts/two-column`))
+    .js(blocks.src(`scripts/blocks/PostContainer/block.js`), blocks.dist(`scripts/post-container`))
+    .js(blocks.src(`scripts/blocks/ProjectContainer/block.js`), blocks.dist(`scripts/project-container`))
+    .js(blocks.src(`scripts/blocks/Squadd/block.js`), blocks.dist(`scripts/squadd`))
+    .js(blocks.src(`scripts/extensions/hide-title-block.js`), blocks.dist(`scripts/extensions`))
 
-  mx.js(theme.script(`editor.js`), theme.dist(`scripts`))
-    .js(blocks.script(`blocks/Banner/block.js`), blocks.dist(`scripts/banner`))
-    .js(blocks.script(`blocks/Container/block.js`), blocks.dist(`scripts/container`))
-    .js(blocks.script(`blocks/FreedomPaper/block.js`), blocks.dist(`scripts/freedom-paper`))
-    .js(blocks.script(`blocks/HorizontalCard/block.js`), blocks.dist(`scripts/horizontal-card`))
-    .js(blocks.script(`blocks/TwoColumn/block.js`), blocks.dist(`scripts/two-column`))
-    .js(blocks.script(`blocks/PostContainer/block.js`), blocks.dist(`scripts/post-container`))
-    .js(blocks.script(`blocks/ProjectContainer/block.js`), blocks.dist(`scripts/project-container`))
-    .js(blocks.script(`blocks/Squadd/block.js`), blocks.dist(`scripts/squadd`))
-    .js(blocks.script(`extensions/hide-title-block.js`), blocks.dist(`scripts/extensions`))
-
-  /**
-   * Assets
-   */
+  /** Copy assets */
   mx.copyWatched(theme.src(`images`), theme.dist(`images`))
     .copyWatched(theme.src(`fonts`), theme.dist(`fonts`))
     .copyWatched(theme.src(`svg`), theme.dist(`svg`))
-    .combine([theme.dist(`styles/gutenberg/style.css`), theme.dist(`styles/app.css`), theme.dist(`styles/public.css`)], theme.dist(`styles/compiled.css`))
 
+  /** Version files */
   mx.sourceMaps(false, 'source-map').inProduction() && mx.version()
 
   return mx;
