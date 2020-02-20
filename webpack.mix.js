d40ddf8b66d12a0976bbfc0cb0b8fd91285b4e59
Kelly Mears <developers@tinypixel.dev>
2020-02-20 14:53:57 -0800

    remove builddump

--- webpack.mix.js
+++ webpack.mix.js
@@ -4,6 +4,8 @@ const onComplete = () => console.log(build.dump())
 
 const build = require('./build/mix')()
 
+/**
 build.then(() => require('fs').writeFile(`./mix.dump.js`, `
   const mixConfig = ${stringify(build.dump(), null, 2)}
 `, onComplete))
+ */
