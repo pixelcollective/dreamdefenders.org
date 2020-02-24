4a9b8bd073a7a60770b0e2b4c8eb162c49a2ee50
Kelly Mears <developers@tinypixel.dev>
2020-02-23 20:18:25 -0800

    fix for url issue

--- config/application.php
+++ config/application.php
@@ -44,9 +44,9 @@ if (file_exists($bedrock . '/.env')) {
  * Base values
  */
 Config::defineSet([
-    'WP_ENV' => Env::get('WP_ENV'),
-    'WP_HOME' => Env::get('WP_HOME'),
-    'WP_SITEURL' => Env::get('WP_SITEURL'),
+    'WP_ENV' => env('WP_ENV'),
+    'WP_HOME' => env('WP_HOME'),
+    'WP_SITEURL' => env('WP_SITEURL'),
 ]);
 
 /**
@@ -69,8 +69,8 @@ Config::defineSet([
     'SCRIPT_DEBUG' => Config::get('WP_ENV') == 'development' ? true : false,
     'DISPLAY_ERRORS' => Config::get('WP_ENV') == 'development' ? true : false,
 ]);
-ini_set('display_errors', Config::get('DISPLAY_ERRORS'));
 
+ini_set('display_errors', Config::get('DISPLAY_ERRORS'));
 
 /**
  * Configure application paths.
@@ -80,7 +80,6 @@ Config::defineSet([
     'WP_CONTENT_DIR' => $web . '/app',
     'WP_CONTENT_URL' => Config::get('WP_HOME') . '/app',
 ]);
-
 
 /**
  * Define environments
@@ -94,13 +93,13 @@ Config::define('ENVIRONMENTS', [
  * Configure DB.
  */
 Config::defineSet([
-    'DB_NAME'      => Env::get('DB_NAME'),
-    'DB_USER'      => Env::get('DB_USER'),
-    'DB_PASSWORD'  => Env::get('DB_PASSWORD'),
-    'DB_HOST'      => Env::get('DB_HOST'),
-    'DB_CHARSET'   => Env::get('DB_CHARSET')   ?: 'utf8',
-    'DB_COLLATION' => Env::get('DB_COLLATION') ?: 'utf8_unicode_ci',
-    'DB_PREFIX'    => Env::get('DB_PREFIX')    ?: 'wp_',
+    'DB_NAME'      => env('DB_NAME'),
+    'DB_USER'      => env('DB_USER'),
+    'DB_PASSWORD'  => env('DB_PASSWORD'),
+    'DB_HOST'      => env('DB_HOST'),
+    'DB_CHARSET'   => env('DB_CHARSET')   ?: 'utf8',
+    'DB_COLLATION' => env('DB_COLLATION') ?: 'utf8_unicode_ci',
+    'DB_PREFIX'    => env('DB_PREFIX')    ?: 'wp_',
 ]);
 $table_prefix = Config::get('DB_PREFIX');
 
@@ -108,25 +107,25 @@ $table_prefix = Config::get('DB_PREFIX');
  * Configure S3.
  */
 Config::defineSet([
-    'S3_UPLOADS_BUCKET'   => Env::get('S3_UPLOADS_BUCKET') ?: 'production',
-    'S3_UPLOADS_KEY'      => Env::get('S3_UPLOADS_KEY'),
-    'S3_UPLOADS_SECRET'   => Env::get('S3_UPLOADS_SECRET'),
-    'S3_UPLOADS_ENDPOINT' => Env::get('S3_UPLOADS_ENDPOINT') ?: 'https://nyc3.digitaloceanspaces.com',
-    'S3_UPLOADS_REGION'   => Env::get('S3_UPLOADS_REGION') ?: 'nyc3',
+    'S3_UPLOADS_BUCKET'   => env('S3_UPLOADS_BUCKET'),
+    'S3_UPLOADS_KEY'      => env('S3_UPLOADS_KEY'),
+    'S3_UPLOADS_SECRET'   => env('S3_UPLOADS_SECRET'),
+    'S3_UPLOADS_ENDPOINT' => env('S3_UPLOADS_ENDPOINT') ?: 'https://nyc3.digitaloceanspaces.com',
+    'S3_UPLOADS_REGION'   => env('S3_UPLOADS_REGION') ?: 'nyc3',
 ]);
 
 /**
  * Configure Redis.
  */
 Config::defineSet([
-    'REDIS_HOST' => Env::get('REDIS_HOST') ?: Config::get('DB_NAME'),
-    'REDIS_AUTH' => Env::get('REDIS_AUTH'),
-    'REDIS_PORT' => Env::get('REDIS_PORT') ?: 25061,
-    'REDIS_OBJECT_CACHE' => Env::get('REDIS_OBJECT_CACHE') ?: true,
+    'REDIS_HOST' => env('REDIS_HOST') ?: Config::get('DB_NAME'),
+    'REDIS_AUTH' => env('REDIS_AUTH'),
+    'REDIS_PORT' => env('REDIS_PORT') ?: 25061,
+    'REDIS_OBJECT_CACHE' => env('REDIS_OBJECT_CACHE') ?: true,
     'PREDIS_CERT' => $bedrock . '/config/cert/redis-ca-cert.crt',
     'PREDIS_VERIFY_PEERS' => true,
-    'WP_CACHE_KEY_SALT' => Env::get('REDIS_CACHE_KEY_SALT') ?: false,
-    'WP_REDIS_USE_CACHE_GROUPS' => Env::get('REDIS_USE_CACHE_GROUPS') ?: false,
+    'WP_CACHE_KEY_SALT' => env('REDIS_CACHE_KEY_SALT') ?: false,
+    'WP_REDIS_USE_CACHE_GROUPS' => env('REDIS_USE_CACHE_GROUPS') ?: false,
 ]);
 
 global $redis_server;
@@ -144,14 +143,14 @@ $redis_server = [
  * Configure auth keys and salts.
  */
 Config::defineSet([
-    'AUTH_KEY' => Env::get('AUTH_KEY') ?: null,
-    'AUTH_SALT' => Env::get('AUTH_SALT') ?: null,
-    'LOGGED_IN_KEY' => Env::get('LOGGED_IN_KEY') ?: null,
-    'LOGGED_IN_SALT' => Env::get('LOGGED_IN_SALT') ?: null,
-    'NONCE_KEY' => Env::get('NONCE_KEY') ?: null,
-    'NONCE_SALT' => Env::get('NONCE_SALT') ?: null,
-    'SECURE_AUTH_KEY' => Env::get('SECURE_AUTH_KEY') ?: null,
-    'SECURE_AUTH_SALT' => Env::get('SECURE_AUTH_SALT') ?: null,
+    'AUTH_KEY' => env('AUTH_KEY') ?: null,
+    'AUTH_SALT' => env('AUTH_SALT') ?: null,
+    'LOGGED_IN_KEY' => env('LOGGED_IN_KEY') ?: null,
+    'LOGGED_IN_SALT' => env('LOGGED_IN_SALT') ?: null,
+    'NONCE_KEY' => env('NONCE_KEY') ?: null,
+    'NONCE_SALT' => env('NONCE_SALT') ?: null,
+    'SECURE_AUTH_KEY' => env('SECURE_AUTH_KEY') ?: null,
+    'SECURE_AUTH_SALT' => env('SECURE_AUTH_SALT') ?: null,
 ]);
 
 /**
@@ -162,10 +161,10 @@ Config::exposeSSL();
 /**
  * Configure Sentry.
  */
-if (Env::get('SENTRY_DSN') && Config::get('WP_ENV') !== 'development') {
+if (env('SENTRY_DSN') && Config::get('WP_ENV') !== 'development') {
     Config::defineSet([
-        'SENTRY_DSN' => Env::get('SENTRY_DSN') ?: null,
-        'RELEASE' => Env::get('GIT_SHA') ?: null,
+        'SENTRY_DSN' => env('SENTRY_DSN') ?: null,
+        'RELEASE' => env('GIT_SHA') ?: null,
     ]);
 
     \Sentry\init([
