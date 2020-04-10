8b6256dd6e05a1fd856b37e6b33b84a33217ba9e
Kelly Mears <code@kellymears.me>
2020-04-10 09:55:16 -0700

    Include CDN rewrite utility (#102)
    * Update composer deps
    
    * commit cdn replacement script
    
    * update .gitignore

---
+++ web/app/mu-plugins/cdn/UrlRewriter.php
@@ -0,0 +1,55 @@
+<?php
+
+class UrlRewriter
+{
+    /**
+     * Constructor.
+     */
+    public function __construct()
+    {
+        $this->cdnBaseUrl = 'https://build.dreamdefenders-c91a.kxcdn.com';
+        $this->baseUrl = get_home_url();
+
+        return $this;
+    }
+
+    /**
+     * Add hooks.
+     */
+    public function runHooks()
+    {
+        add_filter('plugins_url', [$this, 'rewritePlugins']);
+        add_filter('script_loader_src', [$this, 'rewriteCore']);
+        add_filter('style_loader_src', [$this, 'rewriteCore']);
+
+        return true;
+    }
+
+    /**
+     * Serve WP assets from CDN.
+     *
+     * @param $url
+     *
+     * @return mixed
+     */
+    public function rewriteCore($url)
+    {
+        $url = str_replace('/wp/wp-includes', $this->cdnBaseUrl.'/wp/wp-includes', $url);
+
+        return $url;
+    }
+
+    /**
+     * Serve /app/plugins assets from CDN.
+     *
+     * @param $url
+     *
+     * @return mixed
+     */
+    public function rewritePlugins($url)
+    {
+        $url = str_replace($this->baseUrl.'/app/plugins', $this->cdnBaseUrl.'/app/plugins', $url);
+
+        return $url;
+    }
+}
