32a719fe22d0b431dafbc8cc8aa235ba2ab43ad6
Kelly Mears <developers@tinypixel.dev>
2020-06-04 03:33:59 -0700

    update

--- web/app/themes/sage/resources/views/partials/navigation.blade.php
+++ web/app/themes/sage/resources/views/partials/navigation.blade.php
@@ -1,18 +1,18 @@
 @section('toggle-open')
-  <button nav-toggle="open" toggle-target="nav-overlay" class="transition transition-opacity ease-in-out duration-500 inline-flex items-center border-0 text-white px-3 focus:outline-none rounded text-white">
-    @svg('bars', 'menu-icon menu-icon-open w-full fill-current blend-exclusion transition transition-opacity duration-500 ease-in-out', ['width' => '32px', 'height' => '32px'])
+  <button nav-toggle="open" toggle-target="nav-overlay" class="inline-flex items-center w-12 px-3 text-white transition transition-opacity duration-500 ease-in-out border-0 rounded focus:outline-none">
+    @svg('bars', 'menu-icon menu-icon-open w-full fill-current blend-exclusion transition transition-opacity duration-500 ease-in-out', ['width' => '1.5rem', 'height' => '1.5rem'])
   </button>
 @endsection
 
 @section('toggle-close')
-  <button nav-toggle="close" toggle-target="nav-overlay" class="transition transition-opacity ease-in-out duration-500 inline-flex items-center border-0 text-white px-3 focus:outline-none rounded text-white">
-    @svg('times', 'menu-icon menu-icon-close text-white w-full fill-current blend-exclusion transition duration-500 ease-in-out', ['width' => '32px', 'height' => '32px'])
+  <button nav-toggle="close" toggle-target="nav-overlay" class="inline-flex items-center w-12 px-3 text-white transition transition-opacity duration-500 ease-in-out border-0 rounded focus:outline-none">
+    @svg('times', 'menu-icon menu-icon-close text-white w-full fill-current blend-exclusion transition duration-500 ease-in-out', ['width' => '1.5rem', 'height' => '1.5rem'])
   </button>
 @endsection
 
-<header class="bg-opacity-75 bg-black-700 blend-hard-light nav transition transition-opacity ease-in-out duration-500 fixed flex md:hidden z-50 w-full overflow-hidden transition">
-  <div class="flex flex-wrap flex-row items-center justify-between flex-grow p-2 blend-difference">
-    <a href="/" aria-label="Dream Defenders" class="title-font font-medium items-center text-white">
+<header class="fixed z-50 flex w-full overflow-hidden transition transition-opacity duration-500 ease-in-out bg-opacity-75 bg-black-700 blend-hard-light nav md:hidden">
+  <div class="flex flex-row flex-wrap items-center justify-between flex-grow p-2 blend-difference">
+    <a href="/" aria-label="Dream Defenders" class="items-center font-medium text-white title-font">
       @svg('logo', 'w-24 text-white p-2 fill-current', [
         'stroke' => 'currentColor',
         'stroke-linecap' => 'round',
@@ -21,16 +21,15 @@
         'title' => 'The Dream Defenders',
       ])
     </a>
+    @yield('toggle-open')
   </div>
-
-  @yield('toggle-open')
 </header>
 
 
-<header class="bg-black-800 nav transition transition-opacity ease-in-out duration-500 fixed hidden md:flex z-50 w-full overflow-hidden transition">
+<header class="fixed z-50 hidden w-full overflow-hidden transition transition-opacity duration-500 ease-in-out bg-black-800 nav md:flex">
   <div class="w-full blend-hard-screent">
-    <div class="flex flex-wrap flex-row items-center justify-between flex-grow p-2">
-      <a href="/" aria-label="Dream Defenders" hoverfx fx-duration="800"  class="flex title-font font-medium items-center text-white mb-4 md:mb-0">
+    <div class="flex flex-row flex-wrap items-center justify-between flex-grow p-2">
+      <a href="/" aria-label="Dream Defenders" hoverfx fx-duration="800"  class="flex items-center mb-4 font-medium text-white title-font md:mb-0">
         @svg('logo', 'w-24 text-white p-2 fill-current blend-hard-light', [
           'stroke' => 'currentColor',
           'stroke-linecap' => 'round',
@@ -40,12 +39,12 @@
         ])
       </a>
 
-      <nav class="lower-nav flex-grow md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center">
+      <nav class="z-40 flex flex-wrap items-center justify-center flex-grow text-base lower-nav md:ml-auto md:mr-auto">
         <a aria-label="Link to {!! get_bloginfo('site_name') !!} on Facebook" hoverfx fx-duration="800"  class="p-2 mx-1 text-white no-underline rounded rounded-full hover:relative hover:no-underline hover:bg-white hover:text-black" href="https://facebook.com/dreamdefenders">
           @svg('facebook-square', 'w-full fill-current', ['width' => 28, 'height' => 28])
         </a>
 
-        <a aria-label="Link to {!! get_bloginfo('site_name') !!} on Twitter" hoverfx fx-duration="800"  class="p-2 mx-1 text-white no-underline rounded rounded-full  hover:relative hover:no-underline hover:bg-white hover:text-black" href="https://twitter.com/dreamdefenders">
+        <a aria-label="Link to {!! get_bloginfo('site_name') !!} on Twitter" hoverfx fx-duration="800"  class="p-2 mx-1 text-white no-underline rounded rounded-full hover:relative hover:no-underline hover:bg-white hover:text-black" href="https://twitter.com/dreamdefenders">
           @svg('twitter', 'w-full fill-current', ['width' => 28, 'height' => 28])
         </a>
 
@@ -66,7 +65,7 @@
         @endisset
 
         @isset($app->actions[1]->url)
-          <button hoverfx fx-duration="800"  class="px-6 py-2 mx-2 text-sm text-black text-white uppercase bg-transparent border border-white rounded" title="{!! $app->actions[1]->text !!}" aria-label="{!! $app->actions[1]->text !!}">
+          <button hoverfx fx-duration="800" class="px-6 py-2 mx-2 text-sm text-black text-white uppercase bg-transparent border border-white rounded" title="{!! $app->actions[1]->text !!}" aria-label="{!! $app->actions[1]->text !!}">
             <a aria-label="{!! $app->actions[1]->text !!}" href="{!! $app->actions[1]->url !!}">
               {!! $app->actions[1]->text !!}
             </a>
@@ -80,32 +79,36 @@
 </header>
 
 {{-- overlay --}}
-<div class="bg-opacity-25 transition transition-opacity ease-in-out duration-500 fixed nav-overlay flex flex-col h-0 w-full z-40 overflow-hidden fixed top-0 left-0 right-0 bottom-0 bg-black opacity-0">
-  <div class="container h-full">
-    <div class="mx-auto flex flex-wrap p-5 pt-24 flex-col items-center">
-      <div class="relative justify-center w-full mt-4 mb-0 text-center align-middle">
-        <button modal="overlay" hoverfx fx-duration="800"  class="px-6 py-2 mx-2 text-sm text-black uppercase bg-white border border-white rounded" title="{!! $app->actions[0]->text !!}" aria-label="{!! $app->actions[0]->text !!}">
-          <a aria-label="{!! $app->actions[0]->text !!}" href="{!! $app->actions[0]->url !!}">
-            {!! $app->actions[0]->text !!}
-          </a>
-        </button>
+<div class="fixed top-0 bottom-0 left-0 right-0 z-40 flex flex-col w-full h-0 overflow-hidden transition transition-opacity duration-500 ease-in-out bg-black bg-opacity-25 opacity-0 nav-overlay">
+  <div class="h-full">
+    <div class="flex flex-col flex-wrap items-center justify-between px-24 pt-24 mx-auto">
+      <div class="relative justify-center w-1/2 mt-4 mb-0 text-center align-middle">
+        @isset($app->actions[0]->url)
+          <button modal="overlay" hoverfx fx-duration="800"  class="px-6 py-2 mx-2 text-sm text-black uppercase bg-white border border-white rounded" title="{!! $app->actions[0]->text !!}" aria-label="{!! $app->actions[0]->text !!}">
+            <a aria-label="{!! $app->actions[0]->text !!}" href="{!! $app->actions[0]->url !!}">
+              {!! $app->actions[0]->text !!}
+            </a>
+          </button>
+        @endisset
 
-        <button modal="overlay" hoverfx fx-duration="800"  class="px-6 py-2 mx-2 text-sm text-white uppercase bg-transparent border border-white rounded" title="{!! $app->actions[0]->text !!}" aria-label="{!! $app->actions[0]->text !!}">
-          <a aria-label="{!! $app->actions[0]->text !!}" href="{!! $app->actions[0]->url !!}">
-            {!! $app->actions[0]->text !!}
-          </a>
-        </button>
+        @isset($app->actions[1]->url)
+          <button modal="overlay" hoverfx fx-duration="800"  class="px-6 py-2 mx-2 text-sm text-white uppercase bg-transparent border border-white rounded" title="{!! $app->actions[0]->text !!}" aria-label="{!! $app->actions[0]->text !!}">
+            <a aria-label="{!! $app->actions[0]->text !!}" href="{!! $app->actions[0]->url !!}">
+              {!! $app->actions[0]->text !!}
+            </a>
+          </button>
+        @endisset
       </div>
 
       @if ($navigation->about)
-        <div class="relative justify-center w-full mt-4 mb-0 text-center align-middle">
-          <div class="relative justify-center w-64 h-1 mx-auto mt-4 align-middle bg-white" style="height: 2px;"></div>
+        <div class="relative justify-center w-full mt-4 mt-8 mb-0 text-center align-middle max-w-1/2">
+          <div class="relative justify-center h-1 mx-8 mt-4 align-middle bg-white " style="height: 2px;"></div>
           <span class="relative inline px-4 text-sm font-thin text-center text-white bg-black nav-heading md:text-md" style="top: -0.9rem">
             About Us
           </span>
         </div>
 
-        <nav class="relative flex flex-col flex-grow-0 mx-auto text-center justify-middle w-96" style="top: -0.75rem">
+        <nav class="relative flex flex-col flex-grow-0 mx-16 mx-auto text-center justify-middle" style="top: -0.75rem">
           @foreach ($navigation->about as $item)
             <a hoverfx fx-duration="800" fx-on-scale="1.1" fx-off-scale="1" fx-on-translate-y="-3px" fx-off-translate-y="0px" class="flex-grow-0 text-3xl font-bold text-white uppercase md:text-4xl font-display nav-item" href="{!! $item->url !!}">{!! $item->label !!}</a>
           @endforeach
@@ -113,14 +116,14 @@
       @endif
 
       @if ($navigation->vision)
-        <div class="relative justify-center w-full mt-4 mb-0 text-center align-middle">
-          <div class="relative justify-center w-64 h-1 mx-auto mt-4 align-middle bg-white" style="height: 2px;"></div>
-          <span class="relative inline px-4 text-sm font-thin text-center text-white bg-black md:text-md nav-heading" style="top: -0.9rem">
+        <div class="relative justify-center w-full mt-4 mb-0 text-center align-middle max-w-1/2">
+          <div class="relative justify-center h-1 mx-8 mt-4 align-middle bg-white " style="height: 2px;"></div>
+          <span class="relative inline px-4 text-sm font-thin text-center text-white bg-black nav-heading md:text-md" style="top: -0.9rem">
             Our Vision
           </span>
         </div>
 
-        <nav class="relative flex flex-col flex-grow-0 mx-auto text-center justify-middle w-96" style="top: -0.75rem">
+        <nav class="relative flex flex-col flex-grow-0 mx-16 mx-auto text-center justify-middle" style="top: -0.75rem">
           @foreach ($navigation->vision as $item)
             <a hoverfx fx-duration="800" fx-on-scale="1.1" fx-off-scale="1" fx-on-translate-y="-3px" fx-off-translate-y="0px" class="flex-grow-0 text-3xl font-bold text-white uppercase nav-item md:text-4xl font-display" href="{!! $item->url !!}">{!! $item->label !!}</a>
           @endforeach
@@ -128,35 +131,35 @@
       @endif
 
       @if ($navigation->work)
-        <div class="relative justify-center w-full mt-4 mb-0 text-center align-middle">
-          <div class="relative justify-center w-64 h-1 mx-auto mt-4 align-middle bg-white" style="height: 2px;"></div>
+        <div class="relative justify-center w-full mt-4 mb-0 text-center align-middle max-w-1/2">
+          <div class="relative justify-center h-1 mx-8 mt-4 align-middle bg-white " style="height: 2px;"></div>
           <span class="relative inline px-4 text-sm font-thin text-center text-white bg-black nav-heading md:text-md" style="top: -0.9rem">
             Our Work
           </span>
         </div>
 
-        <nav class="relative flex flex-col flex-grow-0 mx-auto text-center justify-middle w-96" style="top: -0.75rem">
+        <nav class="relative flex flex-col flex-grow-0 mx-16 mx-auto text-center justify-middle" style="top: -0.75rem">
           @foreach ($navigation->work as $item)
-            <a hoverfx fx-duration="800" fx-on-scale="1.1" fx-off-scale="1" fx-on-translate-y="-3px" fx-off-translate-y="0px" class="flex-grow-0 text-3xl font-bold text-white uppercase md:text-4xl font-display nav-item text-white" href="{!! $item->url !!}">{!! $item->label !!}</a>
+            <a hoverfx fx-duration="800" fx-on-scale="1.1" fx-off-scale="1" fx-on-translate-y="-3px" fx-off-translate-y="0px" class="flex-grow-0 text-3xl font-bold text-white uppercase md:text-4xl font-display nav-item" href="{!! $item->url !!}">{!! $item->label !!}</a>
           @endforeach
         </nav>
       @endif
     </div>
 
-    <nav class="flex-grow mx-auto mx-auto flex flex-wrap justify-center flex-wrap items-center text-base justify-center">
-      <a aria-label="Link to {!! get_bloginfo('site_name') !!} on Facebook" hoverfx fx-duration="800" fx-on-scale="1.1" fx-off-scale="1" fx-on-translate-y="-3px" fx-off-translate-y="0px" class="p-2 mx-1 text-white no-underline rounded rounded-full hover:relative hover:no-underline hover:bg-white hover:text-black" href="https://facebook.com/dreamdefenders">
+    <nav class="flex flex-row items-center justify-center mx-auto mt-8 text-base">
+      <a aria-label="Link to {!! get_bloginfo('site_name') !!} on Facebook" hoverfx fx-duration="800" fx-on-scale="1.1" fx-off-scale="1" fx-on-translate-y="-3px" fx-off-translate-y="0px" class="block p-2 mx-1 text-white no-underline rounded rounded-full hover:relative hover:no-underline hover:bg-white hover:text-black" href="https://facebook.com/dreamdefenders">
         @svg('facebook-square', 'w-full fill-current', ['width' => 28, 'height' => 28])
       </a>
 
-      <a aria-label="Link to {!! get_bloginfo('site_name') !!} on Twitter" hoverfx fx-duration="800" fx-on-scale="1.1" fx-off-scale="1" fx-on-translate-y="-3px" fx-off-translate-y="0px" class="p-2 mx-1 text-white no-underline rounded rounded-full  hover:relative hover:no-underline hover:bg-white hover:text-black" href="https://twitter.com/dreamdefenders">
+      <a aria-label="Link to {!! get_bloginfo('site_name') !!} on Twitter" hoverfx fx-duration="800" fx-on-scale="1.1" fx-off-scale="1" fx-on-translate-y="-3px" fx-off-translate-y="0px" class="block p-2 mx-1 text-white no-underline rounded rounded-full hover:relative hover:no-underline hover:bg-white hover:text-black" href="https://twitter.com/dreamdefenders">
         @svg('twitter', 'w-full fill-current', ['width' => 28, 'height' => 28])
       </a>
 
-      <a aria-label="Link to {!! get_bloginfo('site_name') !!} on Instagram" hoverfx fx-duration="800" fx-on-scale="1.1" fx-off-scale="1" fx-on-translate-y="-3px" fx-off-translate-y="0px" class="justify-center p-2 mx-1 text-white no-underline align-middle rounded rounded-full hover:relative hover:no-underline hover:bg-white hover:text-black" href="https://instagram.com/thedreamdefenders" style="overflow-x: hidden;">
+      <a aria-label="Link to {!! get_bloginfo('site_name') !!} on Instagram" hoverfx fx-duration="800" fx-on-scale="1.1" fx-off-scale="1" fx-on-translate-y="-3px" fx-off-translate-y="0px" class="justify-center block p-2 mx-1 text-white no-underline align-middle rounded rounded-full hover:relative hover:no-underline hover:bg-white hover:text-black" href="https://instagram.com/thedreamdefenders" style="overflow-x: hidden;">
         @svg('instagram', 'w-full fill-current pl-1', ['width' => 32, 'height' => 32])
       </a>
 
-      <a aria-label="Compose an email to info@dreamdefenders.org" hoverfx fx-duration="800" fx-on-scale="1.1" fx-off-scale="1" fx-on-translate-y="-3px" fx-off-translate-y="0px" class="p-2 mx-1 mr-4 text-white no-underline rounded rounded-full hover:relative hover:no-underline hover:bg-white hover:text-black" href="mailto:info@dreamdefenders.org">
+      <a aria-label="Compose an email to info@dreamdefenders.org" hoverfx fx-duration="800" fx-on-scale="1.1" fx-off-scale="1" fx-on-translate-y="-3px" fx-off-translate-y="0px" class="block p-2 mx-1 mr-4 text-white no-underline rounded rounded-full hover:relative hover:no-underline hover:bg-white hover:text-black" href="mailto:info@dreamdefenders.org">
         @svg('envelope', 'w-full fill-current', ['width' => 28,'height' => 28])
       </a>
     </nav>
@@ -165,7 +168,7 @@
 
 {{--
 
-<nav class="fixed z-50 flex flex-col items-center justify-between w-full overflow-hidden bg-transparent nav transition">
+<nav class="fixed z-50 flex flex-col items-center justify-between w-full overflow-hidden transition bg-transparent nav">
   <div class="container text-black">
     <div class="flex flex-row justify-between w-full">
       <div class="flex items-center justify-middle">
@@ -179,7 +182,7 @@
           @svg('facebook-square', 'w-full fill-current', ['width' => 28, 'height' => 28])
         </a>
 
-        <a aria-label="Link to {!! get_bloginfo('site_name') !!} on Twitter" hoverfx fx-duration="800" fx-on-scale="1.1" fx-off-scale="1" fx-on-translate-y="-3px" fx-off-translate-y="0px" class="p-3 mx-1 text-white no-underline rounded rounded-full  hover:relative hover:no-underline hover:bg-white hover:text-black" href="https://twitter.com/dreamdefenders">
+        <a aria-label="Link to {!! get_bloginfo('site_name') !!} on Twitter" hoverfx fx-duration="800" fx-on-scale="1.1" fx-off-scale="1" fx-on-translate-y="-3px" fx-off-translate-y="0px" class="p-3 mx-1 text-white no-underline rounded rounded-full hover:relative hover:no-underline hover:bg-white hover:text-black" href="https://twitter.com/dreamdefenders">
           @svg('twitter', 'w-full fill-current', ['width' => 28, 'height' => 28])
         </a>
 
--- web/app/themes/sage/webpack.mix.js
+++ web/app/themes/sage/webpack.mix.js
@@ -11,55 +11,59 @@ const script = asset => resolve(__dirname, `resources/assets/scripts/${asset}`);
 /** @dream-defenders/theme conf */
 mx.setPublicPath('./dist')
 
-mx.inProduction()
-  && mx.extract([
+/** Disable purge for now */
+//.purgeCss({
+  const purgeFiles = [
+    'resources/views/*.blade.php',
+    'resources/views/**/*.blade.php',
+    'resources/assets/scripts/*.js',
+    'resources/assets/scripts/**/*.js',
+    'resources/assets/styles/*.css',
+    'resources/assets/styles/*.scss',
+    'resources/assets/styles/**/*.css',
+    'resources/assets/styles/**/*.scss',
+    '../../plugins/dream-defenders-blocks/resources/assets/scripts/*.js',
+    '../../plugins/dream-defenders-blocks/resources/assets/scripts/**/*.js',
+    '../../plugins/dream-defenders-blocks/resources/assets/scripts/blocks/*.js',
+    '../../plugins/dream-defenders-blocks/resources/assets/scripts/blocks/**/*.js',
+    '../../plugins/dream-defenders-blocks/resources/assets/styles/*.js',
+    '../../plugins/dream-defenders-blocks/resources/assets/styles/**/*.js',
+    '../../plugins/dream-defenders-blocks/resources/views/*.blade.php',
+    '../../plugins/dream-defenders-blocks/resources/views/**/*.blade.php',
+  ]
+  const purgeOptions = {
+  whitelist: [
+    'rtl',
+    'home',
+    'blog',
+    'archive',
+    'date',
+    'error404',
+    'logged-in',
+    'admin-bar',
+    'no-customize-support',
+    'custom-background',
+    'wp-custom-logo',
+    'alignnone',
+    'alignright',
+    'alignleft',
+    'wp-caption',
+    'wp-caption-text',
+    'screen-reader-text',
+    'comment-list',
+  ],
+  whitelistPatterns: require('@dream-defenders/mix/purge.config'),
+  whitelistPatternsChildren: require('@dream-defenders/mix/purge.config'),
+}
+
+/** Extract */
+mx.inProduction() && mx.extract([
     'lozad',
     'intersection-observer',
     'animejs',
     'headroom.js',
     '@tinypixelco/hoverfx',
-  ]).purgeCss({
-    globs: [
-      'resources/views/*.blade.php',
-      'resources/views/**/*.blade.php',
-      'resources/assets/scripts/*.js',
-      'resources/assets/scripts/**/*.js',
-      'resources/assets/styles/*.css',
-      'resources/assets/styles/*.scss',
-      'resources/assets/styles/**/*.css',
-      'resources/assets/styles/**/*.scss',
-      '../../plugins/dream-defenders-blocks/resources/assets/scripts/*.js',
-      '../../plugins/dream-defenders-blocks/resources/assets/scripts/**/*.js',
-      '../../plugins/dream-defenders-blocks/resources/assets/scripts/blocks/*.js',
-      '../../plugins/dream-defenders-blocks/resources/assets/scripts/blocks/**/*.js',
-      '../../plugins/dream-defenders-blocks/resources/assets/styles/*.js',
-      '../../plugins/dream-defenders-blocks/resources/assets/styles/**/*.js',
-      '../../plugins/dream-defenders-blocks/resources/views/*.blade.php',
-      '../../plugins/dream-defenders-blocks/resources/views/**/*.blade.php',
-    ],
-    whitelist: [
-      'rtl',
-      'home',
-      'blog',
-      'archive',
-      'date',
-      'error404',
-      'logged-in',
-      'admin-bar',
-      'no-customize-support',
-      'custom-background',
-      'wp-custom-logo',
-      'alignnone',
-      'alignright',
-      'alignleft',
-      'wp-caption',
-      'wp-caption-text',
-      'screen-reader-text',
-      'comment-list',
-    ],
-    whitelistPatterns: require('@dream-defenders/mix/purge.config'),
-    whitelistPatternsChildren: require('@dream-defenders/mix/purge.config'),
-  })
+  ])
 
 /** @dream-defenders/theme scripts */
 mx.js(script`app`, 'work/scripts');
