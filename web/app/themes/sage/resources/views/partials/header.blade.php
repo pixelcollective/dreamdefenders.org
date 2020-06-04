2e5ca22e2dea77baedf328710eda1c54f09ef8e6
Kelly Mears <developers@tinypixel.dev>
2020-06-04 01:52:05 -0700

    update images

--- web/app/themes/sage/resources/views/partials/header.blade.php
+++ web/app/themes/sage/resources/views/partials/header.blade.php
@@ -1,11 +1,11 @@
 <div class="relative w-screen h-full bg-black">
-  <img src="@asset('images/homescreen-bg.png-full.webp')" class="fixed object-cover object-center h-overflow z--100" type="image/webp">
+  <img src="@asset('images/homescreen-bg.png')" class="fixed object-cover object-center h-overflow z--100" type="image/webp">
   <div class="relative w-full bg-gray-600">
     <div class="container flex items-center justify-center mx-auto align-middle">
       <div class="flex items-center w-full h-screen p-24 my-8 text-center text-white lg:p-32">
-        <img src="@asset('images/banner-messaging.png-full.webp')" class="hidden w-full lg:block max-w-4/5" />
-        <img src="@asset('images/banner-messaging-medium.png-full.webp')" class="hidden w-full md:block lg:hidden max-w-3/5" />
-        <img src="@asset('images/banner-messaging-small.png-full.webp')" class="block w-full md:hidden max-w-4/5" />
+        <img src="@asset('images/banner-messaging.png')" class="hidden w-full lg:block max-w-4/5" />
+        <img src="@asset('images/banner-messaging-medium.png')" class="hidden w-full md:block lg:hidden max-w-3/5" />
+        <img src="@asset('images/banner-messaging-small.png')" class="block w-full md:hidden max-w-4/5" />
       </div>
     </div>
   </div>
