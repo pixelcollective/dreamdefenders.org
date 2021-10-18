cb40dfb6b2d9c88c8184f6f4c5bf8ba6b2ba2da9
Kelly Mears <developers@tinypixel.dev>
2021-10-18 15:00:57 -0700

    fix: lightbox window bs

--- web/app/plugins/dream-defenders-modal/resources/assets/scripts/public/Modal.js
+++ web/app/plugins/dream-defenders-modal/resources/assets/scripts/public/Modal.js
@@ -19,20 +19,24 @@ export const Modal = () => {
       <Overlay
         render={() => (
           <div
-            className={`flex flex-align-center h-screen w-screen fixed top-0 left-0 right-0 bottom-0 z-50 bg-white bg-opacity-75`}
+            className={`flex flex-align-center h-screen w-screen fixed top-0 left-0 right-0 bottom-0 z-50 bg-white bg-opacity-75 overflow-y-scroll h-full w-full`}
+            onClick={() => setOpen(false)}
           />
         )}
       />
 
       <div
-        className={`flex flex-align-center fixed top-0 left-0 right-0 bottom-0 z-50`}>
-        <div className="fixed top-1/4 left-8 right-8 bg-white rounded-lg p-4 text-left overflow-hidden shadow-xl mx-auto max-w-lg">
-          <div dangerouslySetInnerHTML={{__html: content}} />
+        className={`flex flex-align-center fixed top-32 md:top-1/4 md:left-8 left-0 right-0 z-50 overflow-y-scroll sm:max-w-lg h-full sm:h-3/5 lg:h-128 max-h-3/5 w-screen mx-auto bg-white rounded-lg shadow-xl  pointer-events-none`}>
+        <div className="md:flex md:flex-col md:justify-between bg-white rounded-lg p-2 md:p-4 w-screen max-w-3xl text-left mx-auto z-50">
+          <div
+            dangerouslySetInnerHTML={{__html: content}}
+            className="pointer-events-auto"
+          />
           <div className="mt-5 sm:mt-6">
             <button
               type="button"
               onClick={() => setOpen(false)}
-              className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-indigo-600 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:col-start-2 sm:text-sm">
+              className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-indigo-600 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:col-start-2 sm:text-sm pointer-events-auto">
               Close
             </button>
           </div>
--- web/app/plugins/dream-defenders-modal/tailwind.config.js
+++ web/app/plugins/dream-defenders-modal/tailwind.config.js
@@ -3,4 +3,19 @@ const {join} = require('path')
 module.exports = {
   jit: true,
   purge: [join(__dirname, 'resources/assets/scripts/**/*')],
+  theme: {
+    extend: {
+      height: {
+        128: '30rem',
+      },
+      maxHeight: {
+        0: '0',
+        '1/4': '25%',
+        '1/2': '50%',
+        '3/4': '75%',
+        80: '80vh',
+        full: '100%',
+      },
+    },
+  },
 }
